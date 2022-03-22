67683b4bf8f1ffb18f4a4c7911a6c72450bd53d5
L <EBogatyrevaPK@yandex.ru>
2022-03-22 19:55:58 +1200

    Интересно Ваше мнение)) смущает меня вот такая конструкция ServiceStation.car.check(car); и public interface ServiceStation {     Bicycle bicycle = new Bicycle();     Truck truck = new Truck();     Car car = new Car();

--- src/pro/sky/java/course2/Car.java
+++ src/pro/sky/java/course2/Car.java
@@ -1,2 +1,31 @@
-package pro.sky.java.course2;public class Car {
+package pro.sky.java.course2;
+
+public class Car implements TypeOfTransport, ServiceStation{
+    private String modelName;
+    private int wheelsCount;
+
+    public void Car(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    @Override
+    public String getModelName() {
+        return modelName;
+    }
+
+    @Override
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @Override
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    @Override
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
 }
--- src/pro/sky/java/course2/Main.java
+++ src/pro/sky/java/course2/Main.java
@@ -3,6 +3,32 @@ package pro.sky.java.course2;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Car car = new Car();
+        Car car2 = new Car();
+        car.setModelName("car1");
+        car2.setModelName("car2");
+        car.setWheelsCount(4);
+        car2.setWheelsCount(4);
+
+        Truck truck = new Truck();
+        Truck truck2 = new Truck();
+        truck.setModelName("truck1");
+        truck2.setModelName("truck2");
+        truck.setWheelsCount(6);
+        truck2.setWheelsCount(8);
+
+        Bicycle bicycle = new Bicycle();
+        Bicycle bicycle2 = new Bicycle();
+        bicycle.setModelName("bicycle1");
+        bicycle2.setModelName("bicycle2");
+        bicycle.setWheelsCount(2);
+        bicycle2.setWheelsCount(2);
+
+        ServiceStation.car.check(car);
+        ServiceStation.car.check(car2);
+        ServiceStation.bicycle.check(bicycle);
+        ServiceStation.bicycle.check(bicycle2);
+        ServiceStation.truck.check(truck);
+        ServiceStation.truck.check(truck2);
     }
 }
